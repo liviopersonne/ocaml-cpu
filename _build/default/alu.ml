72a8f1a12b9887351f1faed8b10c4dc2f5a6406e
Livio Personne <livio.personne@telecom-paris.fr>
2024-10-07 09:22:13 +0200

    fourth commit

--- _build/default/alu.ml
+++
@@ -1,6 +0,0 @@
-(* open Logique *)
-(* open Arithmetique *)
-
-let alu instruction x y = (* instruction sur 3 bits et x y de 16 bits chacun *)
-  let _ = (instruction,x,y) in failwith "ALU not implemented!" ;
-
