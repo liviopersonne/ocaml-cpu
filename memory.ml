13e227321bc9fbd9f9da5fa05434756efbe238ed
Livio Personne <livio.personne@telecom-paris.fr>
2024-10-07 10:12:06 +0200

    Début memoire

--- memory.ml
+++ memory.ml
@@ -1,14 +1,31 @@
-(* open Circuit *)
-(* open Logique *)
+open Circuit
+open Logique
 
-let bit_registre  doit_ecrire valeur_ecrite =
-  let _ = (doit_ecrire,valeur_ecrite) in failwith "Bit registre not implemented!"
+let bit_registre doit_ecrire valeur_ecrite =
+  let _ = (doit_ecrire,valeur_ecrite) in
+  let valeur_stockee = nouvelle_tension() in
+  let nouvelle_valeur = delai (mux doit_ecrire valeur_ecrite valeur_stockee) in
+  relie valeur_stockee nouvelle_valeur;
+  valeur_stockee
   
 let word_registre doit_ecrire valeur_ecrite  =
-  let _ = (doit_ecrire,valeur_ecrite) in failwith "Word registre not implemented!"
-
+  let _ = (doit_ecrire,valeur_ecrite) in
+  let valeur_stockee = Array.init 16 (fun i -> nouvelle_tension()) in
+  let nouvelle_valeur = delai (selecteur doit_ecrire valeur_ecrite valeur_stockee) in
+  relie valeur_stockee nouvelle_valeur;
+  valeur_stockee
  
-let rec memoire taille_addr set l1 l2 e v =
+(* 
+  taill_addr: Taille des adresses (8 bits)
+  set: Ecrire ?
+  l1: 1e adresse de lecture
+  l2: 2e adresse de lecture
+  e: adresse à écrire
+  v: valeur à écrire
+  sortie: les 2 nombres lus
+*)
+let rec memoire (taille_addr: int) (set: tension) (l1: tension list) (l2: tension list)
+  (e: tension list) (v: tension array): tension array * tension array =
   let _ = if false then let _ = memoire taille_addr set l1 l2 e v in () ; () in failwith "Memoire not implemented!"
 
 let rom l1 l2 valeurs =          
