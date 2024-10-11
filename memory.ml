c30a62de78a78e34107808d1386a090c5271cff6
Livio Personne <livio.personne@telecom-paris.fr>
2024-10-11 18:25:16 +0200

    Vrai ram-rom :)

--- memory.ml
+++ memory.ml
@@ -11,14 +11,6 @@ let bit_registre doit_ecrire valeur_ecrite: tension =
 let word_registre doit_ecrire valeur_ecrite: tension array =
   Array.init nb_bits (fun i -> (bit_registre doit_ecrire valeur_ecrite.(i)))
 
-let rom_bit_registre doit_ecrire valeur_ecrite valeur_init: tension =
-  let valeur_stockee = valeur_init in
-  let nouvelle_valeur = mux doit_ecrire valeur_stockee valeur_ecrite in
-  relie valeur_stockee (delai nouvelle_valeur);
-  valeur_stockee
-
-let rom_word_registre doit_ecrire valeur_ecrite valeur_init: tension array =
-  Array.init nb_bits (fun i -> (rom_bit_registre doit_ecrire valeur_ecrite.(i) valeur_init.(i)))
 
 (* 
   taill_addr: Taille des adresses (8 bits)
@@ -64,22 +56,19 @@ let rom (l1: tension list) (l2: tension list)
   
 
 
-let ram_rom (taille_addr: int) (set: tension) (l1: tension list) (l2: tension list)
+(* let ram_rom (set: tension) (l1: tension list) (l2: tension list)
 (e: tension list) (v: tension array) (contenu_rom: tension array array): tension array * tension array =
-  let _ = (taille_addr,set,l1,l2,e,v,contenu_rom) in
-  let nb_valeurs = Array.length contenu_rom in
-  let log2 = log(Float.of_int nb_valeurs) /. log(2.) in
-  if not (Float.is_integer log2) then
-    failwith "Le nombre de valeurs n'est pas une puissance de 2";
-  let taille_addr_rom = Int.of_float log2 in
-  if not (taille_addr = taille_addr_rom) then
-    failwith "La ram et la rom ne font pas la même taille";
+  let taille_rom = Array.length contenu_rom in
+  if not (taille_rom = 256) then
+    failwith "La rom ne fait pas la bonne taille";
+
   let rec construit_ram_rom (taille_addr: int) (set: tension) (rl1: tension list) (rl2: tension list) (addr: int list) (e: tension list)
   (v: tension array): tension array * tension array =
     if taille_addr = 0 then begin
-      let v_rom = contenu_rom.(bit_liste_vers_nb (List.rev addr)) in
-      let case = rom_word_registre set v v_rom in
-      (case, case)
+      let adresse = bit_liste_vers_nb (List.rev addr) in
+      (* let case = rom_word_registre set v v_rom in
+      (case, case) *)
+      ([||], [||])
     end else match (rl1, rl2, e) with
     | (h1::q1, h2::q2, he::qe) -> begin
       let vg1, vg2 = construit_ram_rom (taille_addr-1) (mux set set (neg he)) q1 q2 (0::addr) qe v in
@@ -87,7 +76,17 @@ let ram_rom (taille_addr: int) (set: tension) (l1: tension list) (l2: tension li
       (selecteur h1 vg1 vd1, selecteur h2 vg2 vd2)
     end
     | _ -> failwith "Invalid adresses"
-  in construit_ram_rom taille_addr set l1 l2 [] e v
+  in construit_ram_rom 8 set l1 l2 [] e v *)
+
+let ram_rom (set: tension) (l1: tension list) (l2: tension list)
+(e: tension list) (v: tension array) (contenu_rom: tension array array): tension array * tension array =
+  match (l1, l2) with
+  | h1::q1, h2::q2 -> begin
+    let ramg, ramd = memoire 7 set q1 q2 e v in
+    let romg, romd = rom q1 q2 contenu_rom in
+    (selecteur h1 romg ramg, selecteur h2 romd ramd)
+  end
+  | _ -> failwith "Format des adresses incorrect"
        
     
   
