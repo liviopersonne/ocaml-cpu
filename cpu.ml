4b7aff7ed5f7c9c1b620836b4a7c9dde7acdb6f7
Livio Personne <livio.personne@telecom-paris.fr>
2024-10-11 18:33:09 +0200

    inti cpu

--- cpu.ml
+++ cpu.ml
@@ -4,7 +4,18 @@ open Arithmetique
 open Memory
 open Alu
 
-   
-let cpu program =
+
+
+(* 
+  On arrive enfin au cpu entier !
+  Arguments:
+    program: Tableau d'instructions à exécuter (au plus 256 puisque c'est stocké dans la rom)
+  Sortie:
+    pc: Program Counter
+    opcode: Operation code
+    r2: Registre n°2 (lu)
+    r3: Registre n°3 (lu)
+*)
+let cpu (program: tension array array): tension array * tension array * tension array * tension array =
   let _ = program in failwith "CPU not implemented!"
       
