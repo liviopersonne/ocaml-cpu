a125cbab90a0612d5b4c9dd6d99772925002fda6
Livio Personne <livio.personne@telecom-paris.fr>
2024-10-12 16:35:56 +0200

    ok on a le début

--- cpu.ml
+++ cpu.ml
@@ -25,7 +25,7 @@ let rec relie_liste (l1: tension list) (l2: tension list) =
 
 
 (* La mémoire doit être modifiée que si opcode = 3 *)
-let mem_set opcode: tension = match opcode with
+let memory_set opcode: tension = match opcode with
   | [|a;b;c;d|] -> et (et (a) (b)) (et (neg c) (neg d))  (* Teste si opcode = 3 *)
   | _ -> failwith "Unmatchable case"
 
@@ -41,6 +41,15 @@ let adress_to_register (adress: tension list) (regs: tension array array): tensi
 
 let alu_entries (r2: tension list) (r3: tension list) (regs: tension array array): tension array * tension array = 
   (adress_to_register r2 regs, adress_to_register r3 regs)
+
+let memory_read_adresses (r2: tension list) (r3: tension list): tension list * tension list = 
+  (r2, r3)
+
+let memory_write_adress (r1: tension array) (r2: tension list) (regs: tension array array): tension list = 
+  Array.to_list (somme (adress_to_register r2 regs) r1)
+
+let memory_write_value (r3: tension list) (regs: tension array array): tension array =
+  adress_to_register r3 regs
 
 
 (* 
@@ -61,25 +70,29 @@ let cpu (program: tension array array): tension array * tension array * tension 
   (* Inputs *)
   let input = Array.init 16 (fun _ -> nouvelle_tension()) in
   let opcode = Array.sub input 0 4 in
-  let r1 = Array.to_list (Array.sub input 4 4) in
-  let r2 = Array.to_list (Array.sub input 8 4) in
-  let r3 = Array.to_list (Array.sub input 12 4) in
+  let r1_array = Array.sub input 4 4 in
+  let r2_array = Array.sub input 8 4 in
+  let r3_array = Array.sub input 12 4 in
+  let r1_list = Array.to_list (r1_array) in
+  let r2_list = Array.to_list (r2_array) in
+  let r3_list = Array.to_list (r3_array) in
   
   (* Registers *)
   let pc_set, pc_input, pc_out = register_init(8) in
-  let regs_e = Array.init 16 (fun _ -> nouvelle_tension()) in
-  let regs_v = Array.init 16 (fun _ -> Array.init 8 (fun _ -> nouvelle_tension())) in
-  let regs = Array.init 16 (fun i -> word_registre regs_e.(i) regs_v.(i)) in
+  let regs_set = Array.init 16 (fun _ -> nouvelle_tension()) in
+  let regs_value = Array.init 16 (fun _ -> Array.init 8 (fun _ -> nouvelle_tension())) in
+  let regs = Array.init 16 (fun i -> word_registre regs_set.(i) regs_value.(i)) in
 
 
   (* Memory entries *)
-  let mem_set = mem_set opcode in
-  let mem_l1, mem_l2 = List.init 8 (fun _ -> nouvelle_tension()), List.init 8 (fun _ -> nouvelle_tension()) in
-  let mem_e, mem_v = List.init 8 (fun _ -> nouvelle_tension()), Array.init 8 (fun _ -> nouvelle_tension()) in
+  let mem_set = memory_set opcode in
+  let mem_l1, mem_l2 = memory_read_adresses r2_list r3_list in
+  let mem_e = memory_write_adress r1_array r2_list regs in
+  let mem_v = memory_write_value r3_list regs in
   
   (* ALU entries *)
   let alu_instruction = alu_instruction opcode in
-  let alu_x, alu_y = alu_entries r2 r3 regs in
+  let alu_x, alu_y = alu_entries r2_list r3_list regs in
   
   (* Memory and ALU *)
   let mem1, mem2 = ram_rom mem_set mem_l1 mem_l2 mem_e mem_v program in
